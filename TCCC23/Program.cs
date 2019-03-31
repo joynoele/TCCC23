0d9d12e74cf8525f695bcd4344c202cae6f0fe58
joynoele <joynoele@yahoo.com>
2019-03-31 15:10:08 -0500

    add more demo loggers

--- TCCC23/Program.cs
+++ TCCC23/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using Serilog;
 
-namespace TCCC23
+namespace TCCC23.Console
 {
     class Program
     {
+        private static Serilog.ILogger _log;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            _log = DemoLoggers.EnrichedMachineNameConsole();
+            TestMessages();
+
+            System.Console.ReadKey();
         }
+
+        static void TestMessages()
+        {
+            _log.Verbose("hello verbose!");
+            _log.Debug("hello debug!");
+            _log.Information("hello information!");
+            _log.Warning("hello warning!");
+            _log.Error("hello error!");
+            _log.Fatal("hello fatal!");
+        }
+
+
     }
 }
